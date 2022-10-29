f8fe32f2ef6da2f4248421b7fe82f5e6d72ce5eb
haphihung <haphihung55@gmail.com>
2022-10-29 22:48:38 +0700

    Add sample data for employee info data grid

---
+++ BusinessManagementApp/Data/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessManagementApp.Data
+{
+    public class Employee
+    {
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public string Gender { get; set; } = "";
+        public DateTime BirthDate { get; set; } = new();
+        public string Position { get; set; } = "";
+        public string Department { get; set; } = "";
+    }
+}
---
+++ BusinessManagementApp/ViewModels/EmployeeInfoVM.cs
@@ -0,0 +1,30 @@
+﻿using BusinessManagementApp.Data;
+using BusinessManagementApp.Repositories;
+using BusinessManagementApp.Utils;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessManagementApp.ViewModels
+{
+    public class EmployeeInfoVM : ObservableObject
+    {
+        public EmployeesRepository employeesRepository = new();
+
+        public ObservableCollection<Employee> Employees { get; set; } = new();
+
+        public EmployeeInfoVM()
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Employees.AddRange(employeesRepository.GetAllEmployees());
+        }
+    }
+}
