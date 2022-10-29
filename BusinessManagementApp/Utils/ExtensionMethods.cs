62b416d1c08a90b9f143a118a5c99da19ccd36df
haphihung <haphihung55@gmail.com>
2022-10-29 22:47:56 +0700

    Add extension method to add all items of a IEnumerable into an ObservableCollection

---
+++ BusinessManagementApp/Utils/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace BusinessManagementApp.Utils
+{
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// Add all objects from items to the end of ObservableCollection.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="observableCollection">Collection to add into</param>
+        /// <param name="items">Items to add</param>
+        public static void AddRange<T>(this ObservableCollection<T> observableCollection, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                observableCollection.Add(item);
+            }
+        }
+    }
+}
