b23538a3069b539bd84394761a59525beed4875f
haphihung <haphihung55@gmail.com>
2022-10-29 23:39:44 +0700

    Add more views

---
+++ BusinessManagementApp/ViewModels/WorkspaceVM.cs
@@ -0,0 +1,58 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessManagementApp.ViewModels
+{
+    public enum SidebarNavPageNames
+    {
+        [Description("Overview")]
+        Overview,
+        [Description("Orders")]
+        Orders,
+        [Description("Employee info")]
+        EmployeeInfo
+    }
+
+    public class WorkspaceVM : ObservableObject
+    {
+        private ObservableObject currentViewVM = App.Current.Services.GetRequiredService<OverviewVM>();
+        public ObservableObject CurrentViewVM
+        {
+            get => currentViewVM;
+            set => SetProperty(ref currentViewVM, value);
+        }
+
+        private SidebarNavPageNames selectedViewName = SidebarNavPageNames.EmployeeInfo;
+        public SidebarNavPageNames SelectedViewName
+        {
+            get => selectedViewName;
+            set
+            {
+                SetProperty(ref selectedViewName, value);
+                ChangeView();
+            }
+        }
+
+        private void ChangeView()
+        {
+            switch (SelectedViewName)
+            {
+                case SidebarNavPageNames.Overview:
+                    CurrentViewVM = App.Current.Services.GetRequiredService<OverviewVM>();
+                    break;
+                case SidebarNavPageNames.Orders:
+                    CurrentViewVM = App.Current.Services.GetRequiredService<OrdersVM>();
+                    break;
+                case SidebarNavPageNames.EmployeeInfo:
+                    CurrentViewVM = App.Current.Services.GetRequiredService<EmployeeInfoVM>();
+                    break;
+            }
+        }
+    }
+}
