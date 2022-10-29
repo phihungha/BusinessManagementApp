fee3087a652a73d46ac73e7381983ea074a2c67b
haphihung <haphihung55@gmail.com>
2022-10-29 23:37:40 +0700

    Allow binding controls with enum

---
+++ BusinessManagementApp/Utils/EnumComboBoxHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+
+namespace BusinessManagementApp.Utils
+{
+    /// <summary>
+    /// Help bind ComboBox into enum values
+    /// Source: https://stackoverflow.com/a/12430331/15872481
+    /// </summary>
+    public static class EnumComboBoxHelper
+    {
+        public static string Description(this Enum value)
+        {
+            var attributes = value.GetType()
+                ?.GetField(value.ToString())
+                ?.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if (attributes != null && attributes.Any())
+            {
+                var attribute = attributes.First() as DescriptionAttribute;
+                if (attribute != null)
+                    return attribute.Description;
+            }
+
+            // If no description is found, the least we can do is replace underscores with spaces
+            // You can add your own custom default formatting logic here
+            TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
+            return ti.ToTitleCase(ti.ToLower(value.ToString().Replace("_", " ")));
+        }
+
+        public static IEnumerable<ValueDescription> GetAllValuesAndDescriptions(Type t)
+        {
+            if (!t.IsEnum)
+                throw new ArgumentException($"{nameof(t)} must be an enum type");
+
+            return Enum.GetValues(t)
+                .Cast<Enum>()
+                .Select((e) => new ValueDescription() { Value = e, Description = e.Description() })
+                .ToList();
+        }
+    }
+
+    public class ValueDescription
+    {
+        public object Value { get; set; } = new();
+        public object Description { get; set; } = new();
+    }
+}
---
+++ BusinessManagementApp/ViewModels/Converters/EnumToCollection.cs
@@ -0,0 +1,28 @@
+﻿using BusinessManagementApp.Utils;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace BusinessManagementApp.ViewModels.Converters
+{
+    [ValueConversion(typeof(Enum), typeof(IEnumerable<ValueDescription>))]
+    public class EnumToCollection : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return EnumComboBoxHelper.GetAllValuesAndDescriptions(value.GetType());
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
