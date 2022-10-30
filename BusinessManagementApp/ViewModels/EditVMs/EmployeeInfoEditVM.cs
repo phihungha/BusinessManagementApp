4746eb52bca8f2b0d1aedb41007e9a3181a40061
haphihung <haphihung55@gmail.com>
2022-10-30 18:09:04 +0700

    Add navigation from edit to list view

--- BusinessManagementApp/ViewModels/EditVMs/EmployeeInfoEditVM.cs
+++ BusinessManagementApp/ViewModels/EditVMs/EmployeeInfoEditVM.cs
@@ -1,14 +1,24 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using BusinessManagementApp.ViewModels.Utils;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace BusinessManagementApp.ViewModels.EditVMs
 {
     public class EmployeeInfoEditVM : ObservableObject
     {
+        public ICommand Cancel { get; private set; }
 
+        public EmployeeInfoEditVM()
+        {
+            Cancel = new RelayCommand(
+                () => WorkspaceNavUtils.ChangeView(WorkspaceViewName.EmployeeInfo)
+                );
+        }
     }
 }
