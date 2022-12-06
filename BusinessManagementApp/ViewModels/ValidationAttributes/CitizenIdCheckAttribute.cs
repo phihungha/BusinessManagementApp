f91b0369c037edfb7c176209434fb9deb73bd0d6
haphihung <haphihung55@gmail.com>
2022-12-06 22:15:38 +0700

    Add useful custom validation attributes

---
+++ BusinessManagementApp/ViewModels/ValidationAttributes/CitizenIdCheckAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace BusinessManagementApp.ViewModels.ValidationAttributes
+{
+    public sealed class CitizenIdCheckAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new("Invalid citizen ID");
+            }
+
+            string citizenId = (string)value;
+            if (citizenId.Length == 12 && citizenId.All(char.IsDigit))
+            {
+                return ValidationResult.Success;
+            }
+            return new("Invalid citizen ID");
+        }
+    }
+}
---
+++ BusinessManagementApp/ViewModels/ValidationAttributes/PhoneNumberCheckAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace BusinessManagementApp.ViewModels.ValidationAttributes
+{
+    public sealed class PhoneNumberCheckAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new("Invalid phone number");
+            }
+
+            string phoneNumber = (string)value;
+            if (phoneNumber.Length == 10 && phoneNumber.All(char.IsDigit))
+            {
+                return ValidationResult.Success;
+            }
+            return new("Invalid phone number");
+        }
+    }
+}
