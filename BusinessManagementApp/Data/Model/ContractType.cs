31795f531a45a18c8b0f0fa6aacb06f71b633a3c
Ha Phi Hung <haphihung55@gmail.com>
2022-12-09 20:11:04 +0700

    Use string keyword instead of objects

--- BusinessManagementApp/Data/Model/ContractType.cs
+++ BusinessManagementApp/Data/Model/ContractType.cs
@@ -1,19 +1,15 @@
-﻿using System;
-
-namespace BusinessManagementApp.Data.Model
+﻿namespace BusinessManagementApp.Data.Model
 {
     public class ContractType
     {
+        public string Id { get; set; }
 
-        public String Id { get; set; }
+        public string Name { get; set; }
 
-        public String Name { get; set; }
-
-        public String Description { get; set; }
+        public string Description { get; set; }
 
         public decimal BaseSalary { get; set; }
 
         public int Period { get; set; }
-
     }
 }
