17abac6740f6398b15b5c861ce09df45c31eba92
Acj16 <114270968+Acj16@users.noreply.github.com>
2022-12-14 13:08:14 +0700

    add contract type (xaml , repo, vm)

--- BusinessManagementApp/Data/ContractTypesRepo.cs
+++ BusinessManagementApp/Data/ContractTypesRepo.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿using BusinessManagementApp.Data.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BusinessManagementApp.Data
 {
-    internal class ContractTypesRepo
+    public class ContractTypesRepo
     {
+        public IObservable<object> DeleteContractType(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public IObservable<ContractType> GetContractType(string name)
+        {
+            var contracttype = new ContractType()
+            {
+                Id = 1,
+                Name = "Permanent",
+                BaseSalary = 60,
+                Period = new DateTime(2022, 12, 30)
+            };
+            return Observable.FromAsync(() => Task.FromResult(contracttype));
+        }
+        public IObservable<List<ContractType>> GetContractTypes()
+        {
+            var contracttypes = new List<ContractType>()
+            {
+                new ContractType()
+                {
+                    Id = 1,
+                    Name = "1 month",
+                    BaseSalary = 60,
+                    Period= new DateTime(2022, 12, 30)
+                },
+                new ContractType()
+                {
+                    Id = 2,
+                    Name = "2 month",
+                    BaseSalary = 50,
+                    Period= new DateTime(2022, 12, 31)
+                }
+            };
+            return Observable.FromAsync(() => Task.FromResult(contracttypes));
+        }
+        public IObservable<ContractType> AddContractType(ContractType contracttype)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<ContractType> UpdateContractType(string contracttypeId, ContractType contracttype)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- BusinessManagementApp/ViewModels/ContractTypesVM.cs
+++ BusinessManagementApp/ViewModels/ContractTypesVM.cs
@@ -1,15 +1,87 @@
-﻿using BusinessManagementApp.ViewModels.Utils;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using BusinessManagementApp.Data;
+using BusinessManagementApp.Data.Model;
+using BusinessManagementApp.Utils;
+using BusinessManagementApp.ViewModels.Utils;
+using BusinessManagementApp.Views;
+using CommunityToolkit.Mvvm.Input;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using System.Windows.Data;
+using System.Windows.Input;
 
 namespace BusinessManagementApp.ViewModels
 {
+    public enum ContractTypeInfoSearchBy
+    {
+        [Description("ID")]
+        Id,
+
+        [Description("Name")]
+        Name,
+    }
     public class ContractTypesVM : ViewModelBase
     {
+        private ContractTypesRepo contracttypesRepo;
 
+        private ObservableCollection<ContractType> contracttypes { get; } = new();
+
+        public ICollectionView ContractTypesView { get; }
+
+        public string SearchText { get; set; } = string.Empty;
+
+        public ContractTypeInfoSearchBy SearchBy { get; set; } = ContractTypeInfoSearchBy.Name;
+
+        public ICommand AddContractType { get; }
+        public ICommand Search { get; }
+        public ICommand Edit { get; }
+        public ContractTypesVM(ContractTypesRepo contracttypesRepo)
+        {
+            this.contracttypesRepo = contracttypesRepo;
+
+            // DataGrid accesses the ObservableCollection of model objects
+            // indirectly via a ICollectionView to support filtering.
+            var collectionViewSource = new CollectionViewSource() { Source = contracttypes };
+            ContractTypesView = collectionViewSource.View;
+            ContractTypesView.Filter = FilterList;
+
+            AddContractType = new RelayCommand(() => WorkspaceNavUtils.NavigateTo(WorkspaceViewName.ContractTypeDetails));
+            Search = new RelayCommand(() => ContractTypesView.Refresh());
+            Edit = new RelayCommand<string>(id => OpenDetailsView(id));
+
+            LoadData();
+        }
+        private bool FilterList(object item)
+        {
+            var contracttype = (ContractType)item;
+
+            switch (SearchBy)
+            {
+                case ContractTypeInfoSearchBy.Name:
+                    return contracttype.Name.Contains(SearchText, StringComparison.InvariantCultureIgnoreCase);
+
+                case ContractTypeInfoSearchBy.Id:
+                    return contracttype.Id.ToString().Contains(SearchText, StringComparison.InvariantCultureIgnoreCase);
+
+                default:
+                    return false;
+            }
+        }
+        private void OpenDetailsView(string? id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentException("ID is null");
+            }
+
+            WorkspaceNavUtils.NavigateToWithExtra(WorkspaceViewName.ContractTypeDetails, id);
+        }
+
+        private async void LoadData()
+        {
+            contracttypes.AddRange(await contracttypesRepo.GetContractTypes());
+        }
     }
+
 }
