aeff460d5cb411f94d900526b4e720c19c43a49e
dangthuanvo <20520314@gm.uit.edu.vn>
2022-12-14 20:42:54 +0700

    add department UI

--- BusinessManagementApp/Data/DepartmentsRepo.cs
+++ BusinessManagementApp/Data/DepartmentsRepo.cs
@@ -8,6 +8,24 @@ namespace BusinessManagementApp.Data
 {
     public class DepartmentsRepo
     {
+        public IObservable<object> DeleteDepartment(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<Department> GetDepartment(int id)
+        {
+            var department = new Department()
+            {
+                Id = 1,
+                Name = "Phong tai chinh",
+                Head = new Employee() { Id = "1", Name = "Vo Dang Thuan" },
+                PhoneNumber = "0123456789"
+            };
+
+            return Observable.FromAsync(() => Task.FromResult(department));
+        }
+
         public IObservable<List<Department>> GetDepartments()
         {
             var departments = new List<Department>()
@@ -15,21 +33,30 @@ namespace BusinessManagementApp.Data
                 new Department()
                 {
                     Id = 1,
-                    Name = "Sales",
+                    Name = "Phong tai chinh",
+                    Head = new Employee() { Id = "1", Name = "Vo Dang Thuan" },
+                    PhoneNumber = "0123456789"
                 },
                 new Department()
                 {
                     Id = 2,
-                    Name = "Human resources",
-                },
-                new Department()
-                {
-                    Id = 3,
-                    Name = "IT",
+                    Name = "Phong ke toan",
+                    Head = new Employee() { Id = "2", Name = "Vo Dang Khoa" },
+                    PhoneNumber = "0123456788"
                 }
-            };
 
+        };
             return Observable.FromAsync(() => Task.FromResult(departments));
+        }
+
+        public IObservable<Employee> AddDepartment(Department department)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<Department> UpdateDepartment(int departmentId, Department department)
+        {
+            throw new NotImplementedException();
         }
     }
 }
