f1fbd8ed4eddd40e27435586b3afbe46368650b9
dangthuanvo <20520314@gm.uit.edu.vn>
2022-12-15 11:17:13 +0700

    added voucher type UI

--- BusinessManagementApp/ViewModels/DetailsVMs/VoucherTypeDetailsVM.cs
+++ BusinessManagementApp/ViewModels/DetailsVMs/VoucherTypeDetailsVM.cs
@@ -1,13 +1,204 @@
-﻿using BusinessManagementApp.ViewModels.Utils;
+﻿using BusinessManagementApp.Data;
+using BusinessManagementApp.Data.Model;
+using BusinessManagementApp.ViewModels.Utils;
+using BusinessManagementApp.Utils;
+using BusinessManagementApp.ViewModels.ValidationAttributes;
+using CommunityToolkit.Mvvm.Input;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using BusinessManagementApp.Views;
+using System.Windows.Documents;
+using System.Collections.Generic;
 
 namespace BusinessManagementApp.ViewModels.DetailsVMs
 {
     public class VoucherTypeDetailsVM : ViewModelBase
     {
+        // Declare dependencies such as repositories here.
+        #region Dependencies
+
+        private VoucherTypesRepo voucherTypesRepo;
+        private ProductsRepo productsRepo;
+
+        #endregion Dependencies
+
+        #region Combobox items
+        public ObservableCollection<Product> Products { get; } = new();
+        #endregion
+
+
+        // Properties for inputs on the screen
+        // Remember to declare validation attributes when appropriate.
+        // List of validation attributes: https://learn.microsoft.com/en-us/dotnet/api/system.componentmodel.dataannotations?view=net-7.0
+        // Check ViewModels/ValidationAttributes.cs for custom validation attributes.
+        #region Input properties
+
+        private int id = 0;
+
+        public int Id
+        {
+            get => id;
+            private set => SetProperty(ref id, value);
+        }
+
+        private string name = string.Empty;
+
+        [Required]
+        public string Name
+        {
+            get => name;
+            set => SetProperty(ref name, value);
+        }
+
+        private string description = string.Empty;
+        public string Description
+        {
+            get => description;
+            set => SetProperty(ref description, value, true);
+        }
+
+
+        private DiscountType discountType = DiscountType.Percent;
+
+        public DiscountType DiscountType
+        {
+            get => discountType;
+            set => SetProperty(ref discountType, value, true);
+        }
+
+        private double discountValue = 0;
+
+        public double DiscountValue
+        {
+            get => discountValue;
+            set => SetProperty(ref discountValue, value, true);
+        }
+
+        private List<Product> appliedProducts = new List<Product>();
+        public List<Product> AppliedProducts
+        {
+            get => appliedProducts;
+            set => SetProperty(ref appliedProducts, value, true);
+        }
+
+        #endregion Input properties
+
+        #region Button enable/disable logic
+
+        private bool isEditMode = false;
+
+        private bool IsEditMode
+        {
+            get => isEditMode;
+            set
+            {
+                SetProperty(ref isEditMode, value);
+                CanDelete = value;
+            }
+        }
+
+        private bool canSave = false;
+
+        public bool CanSave
+        {
+            get => canSave;
+            private set => SetProperty(ref canSave, value);
+        }
+
+        private bool canDelete = false;
+
+        public bool CanDelete
+        {
+            get => canDelete;
+            private set => SetProperty(ref canDelete, value);
+        }
+
+        #endregion Button enable/disable logic
+
+        #region Commands for buttons
+
+        public ICommand Save { get; private set; }
+        public ICommand Delete { get; private set; }
+        public ICommand Cancel { get; private set; }
+
+        #endregion Commands for buttons
+
+        // Declare dependencies (e.g repositories) as constructor parameters
+        // Go into Startup.cs to add new depencencies if needed
+        public VoucherTypeDetailsVM(VoucherTypesRepo voucherTypesRepo, ProductsRepo productsRepo)
+        {
+            this.voucherTypesRepo = voucherTypesRepo;
+            this.productsRepo = productsRepo;
+
+            Save = new AsyncRelayCommand(SaveVoucherType);
+            Delete = new AsyncRelayCommand(DeleteVoucherType);
+            Cancel = new RelayCommand(
+                () => WorkspaceNavUtils.NavigateTo(WorkspaceViewName.VoucherTypes)
+                );
+        }
+
+        // Load data from repositories here.
+        // An object passed when navigating to this screen is also received here.
+        public override async void LoadData(object? id = null)
+        {
+            //Products.AddRange(await productsRepo.GetProducts());
+
+            if (id != null)
+            {
+                IsEditMode = true;
+                await LoadVoucherType((int)id);
+            }
+
+            CanSave = true;
+        }
+
+        private async Task LoadVoucherType(int id)
+        {
+            VoucherType voucherType = await voucherTypesRepo.GetVoucherType(id);
+            Id = voucherType.Id;
+            Name = voucherType.Name;
+            DiscountType = voucherType.DiscountType;    
+            DiscountValue = voucherType.DiscountValue;
+            AppliedProducts = voucherType.AppliedProducts;
+        }
+
+        private async Task SaveVoucherType()
+        {
+            ValidateAllProperties();
+            if (HasErrors)
+                return;
+
+            var voucherType = new VoucherType()
+            {
+                Id = Id,
+                Name = Name,
+                Description = Description, 
+                DiscountValue = DiscountValue,
+                AppliedProducts =AppliedProducts,
+                DiscountType = DiscountType,
+            };
+
+            if (IsEditMode)
+            {
+                await voucherTypesRepo.UpdateVoucherType(Id, voucherType);
+            }
+            else
+            {
+                await voucherTypesRepo.AddVoucherType(voucherType);
+            }
+
+            // Navigate back to list screen
+            WorkspaceNavUtils.NavigateTo(WorkspaceViewName.VoucherTypes);
+        }
+
+        private async Task DeleteVoucherType()
+        {
+            await voucherTypesRepo.DeleteVoucherType(Id);
+            WorkspaceNavUtils.NavigateTo(WorkspaceViewName.VoucherTypes);
+        }
     }
 }
--- BusinessManagementApp/ViewModels/VoucherTypesVM.cs
+++ BusinessManagementApp/ViewModels/VoucherTypesVM.cs
@@ -1,15 +1,95 @@
-﻿using BusinessManagementApp.ViewModels.Utils;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using BusinessManagementApp.Data;
+using BusinessManagementApp.Data.Model;
+using BusinessManagementApp.Utils;
+using BusinessManagementApp.ViewModels.Utils;
+using BusinessManagementApp.Views;
+using CommunityToolkit.Mvvm.Input;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using System.Windows.Data;
+using System.Windows.Input;
 
 namespace BusinessManagementApp.ViewModels
 {
+    public enum VoucherTypeInfoSearchBy
+    {
+        [Description("ID")]
+        Id,
+
+        [Description("Name")]
+        Name,
+
+    }
+
     public class VoucherTypesVM : ViewModelBase
     {
+        private VoucherTypesRepo voucherTypesRepo;
 
+        private ObservableCollection<VoucherType> voucherTypes { get; } = new();
+
+        public ICollectionView VoucherTypesView { get; }
+
+        public string SearchText { get; set; } = string.Empty;
+
+        public VoucherTypeInfoSearchBy SearchBy { get; set; } = VoucherTypeInfoSearchBy.Name;
+
+        public ICommand AddVoucherType { get; }
+        public ICommand Search { get; }
+        public ICommand Edit { get; }
+
+        // Declare dependencies (e.g repositories) to use as constructor parameters
+        // Go into Startup.cs to add new depencencies if needed
+        public VoucherTypesVM(VoucherTypesRepo voucherTypesRepo)
+        {
+            this.voucherTypesRepo = voucherTypesRepo;
+
+            // DataGrid accesses the ObservableCollection of model objects
+            // indirectly via a ICollectionView to support filtering.
+            var collectionViewSource = new CollectionViewSource() { Source = voucherTypes };
+            VoucherTypesView = collectionViewSource.View;
+            VoucherTypesView.Filter = FilterList;
+
+            AddVoucherType = new RelayCommand(() => WorkspaceNavUtils.NavigateTo(WorkspaceViewName.VoucherTypeDetails));
+            Search = new RelayCommand(() => VoucherTypesView.Refresh());
+            Edit = new RelayCommand<int?>(id => OpenDetailsView(id));
+
+            LoadData();
+        }
+
+        private bool FilterList(object item)
+        {
+            var voucherType = (VoucherType)item;
+
+            switch (SearchBy)
+            {
+                case VoucherTypeInfoSearchBy.Name:
+                    return voucherType.Name.Contains(SearchText, StringComparison.InvariantCultureIgnoreCase);
+
+                case VoucherTypeInfoSearchBy.Id:
+                    return voucherType.Id.ToString().Contains(SearchText, StringComparison.InvariantCultureIgnoreCase);
+
+
+                default:
+                    return false;
+            }
+        }
+
+        private void OpenDetailsView(int? id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentException("ID is null");
+            }
+
+            // Navigate to details screen
+            WorkspaceNavUtils.NavigateToWithExtra(WorkspaceViewName.VoucherTypeDetails, id);
+        }
+
+        private async void LoadData()
+        {
+            voucherTypes.AddRange(await voucherTypesRepo.GetVoucherTypes());
+        }
     }
 }
