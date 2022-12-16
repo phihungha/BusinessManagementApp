156ffb21757dd4e2678f986a7f613bdc0eae30db
Trung Lê Quang <20520333@ms.uit.edu.vn>
2022-12-15 22:40:09 -0800

    add Manage Customers

--- BusinessManagementApp/ViewModels/CustomersVM.cs
+++ BusinessManagementApp/ViewModels/CustomersVM.cs
@@ -1,15 +1,107 @@
-﻿using BusinessManagementApp.ViewModels.Utils;
+﻿using BusinessManagementApp.Data.Model;
+using BusinessManagementApp.Data;
+using BusinessManagementApp.ViewModels.Utils;
 using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using BusinessManagementApp.Views;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Data;
+using System.Reactive.Linq;
+using BusinessManagementApp.Utils;
 
 namespace BusinessManagementApp.ViewModels
 {
+    public enum CustomerInfoSearchBy
+    {
+        [Description("ID")]
+        Id,
+
+        [Description("Name")]
+        Name,
+
+        [Description("Address")]
+        Address,
+
+        [Description("Phone")]
+        Phone
+    }
     public class CustomersVM : ViewModelBase
     {
+        private CustomersRepo customersRepo;
 
+        private ObservableCollection<Customer> customers { get; } = new();
+
+        public ICollectionView CustomersView { get; }
+
+        public string SearchText { get; set; } = string.Empty;
+
+        public CustomerInfoSearchBy SearchBy { get; set; } = CustomerInfoSearchBy.Name;
+
+        public ICommand AddCustomer { get; }
+        public ICommand Search { get; }
+        public ICommand Edit { get; }
+
+        public CustomersVM(CustomersRepo customersRepo)
+        {
+            this.customersRepo = customersRepo;
+
+            // DataGrid accesses the ObservableCollection of model objects
+            // indirectly via a ICollectionView to support filtering.
+            var collectionViewSource = new CollectionViewSource() { Source = customers };
+            CustomersView = collectionViewSource.View;
+            CustomersView.Filter = FilterList;
+
+            AddCustomer = new RelayCommand(() => WorkspaceNavUtils.NavigateTo(WorkspaceViewName.CustomerDetails));
+            Search = new RelayCommand(() => CustomersView.Refresh());
+            Edit = new RelayCommand<string>(id => OpenDetailsView(id));
+
+            LoadData();
+        }
+
+        private bool FilterList(object item)
+        {
+            var customer = (Customer)item;
+
+            switch (SearchBy)
+            {
+                case CustomerInfoSearchBy.Name:
+                    return customer.Name.Contains(SearchText, StringComparison.InvariantCultureIgnoreCase);
+
+                case CustomerInfoSearchBy.Id:
+                    return customer.Id.Contains(SearchText, StringComparison.InvariantCultureIgnoreCase);
+
+                case CustomerInfoSearchBy.Address:
+                    return customer.Address.Contains(SearchText, StringComparison.InvariantCultureIgnoreCase);
+
+                case CustomerInfoSearchBy.Phone:
+                    return customer.Phone.Contains(SearchText, StringComparison.InvariantCultureIgnoreCase);
+
+                default:
+                    return false;
+            }
+        }
+
+        private void OpenDetailsView(string? id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentException("ID is null");
+            }
+
+            // Navigate to details screen
+            WorkspaceNavUtils.NavigateToWithExtra(WorkspaceViewName.CustomerDetails, id);
+        }
+
+        private async void LoadData()
+        {
+            customers.AddRange(await customersRepo.GetCustomers());
+        }
     }
 }
--- BusinessManagementApp/ViewModels/DetailsVMs/CustomerDetailsVM.cs
+++ BusinessManagementApp/ViewModels/DetailsVMs/CustomerDetailsVM.cs
@@ -1,13 +1,198 @@
-﻿using BusinessManagementApp.ViewModels.Utils;
+﻿using BusinessManagementApp.Data;
+using BusinessManagementApp.Data.Model;
+using BusinessManagementApp.ViewModels.Utils;
+using BusinessManagementApp.ViewModels.ValidationAttributes;
+using BusinessManagementApp.Views;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace BusinessManagementApp.ViewModels.DetailsVMs
 {
     public class CustomerDetailsVM : ViewModelBase
     {
+        #region Dependencies
+
+        private CustomersRepo customersRepo;
+
+        #endregion Dependencies
+        #region Input properties
+
+        private string id = string.Empty;
+
+        public string Id
+        {
+            get => id;
+            private set => SetProperty(ref id, value);
+        }
+
+        private string name = string.Empty;
+
+        [Required]
+        public string Name
+        {
+            get => name;
+            set => SetProperty(ref name, value);
+        }
+
+        private Gender gender = Gender.Male;
+
+        public Gender Gender
+        {
+            get => gender;
+            set => SetProperty(ref gender, value, true);
+        }
+
+        private DateTime birthday = new DateTime(2000, 1, 1);
+
+        public DateTime Birthday
+        {
+            get => birthday;
+            set => SetProperty(ref birthday, value, true);
+        }
+
+        private string email = string.Empty;
+
+        [EmailAddress]
+        public string Email
+        {
+            get => email;
+            set => SetProperty(ref email, value, true);
+        }
+
+        private string phone = string.Empty;
+
+        [PhoneNumber]
+        public string Phone
+        {
+            get => phone;
+            set => SetProperty(ref phone, value, true);
+        }
+
+        private string address = string.Empty;
+
+        [Required]
+        public string Address
+        {
+            get => address;
+            set => SetProperty(ref address, value, true);
+        }
+
+        #endregion Input properties
+        #region Button enable/disable logic
+
+        private bool isEditMode = false;
+
+        private bool IsEditMode
+        {
+            get => isEditMode;
+            set
+            {
+                SetProperty(ref isEditMode, value);
+                CanDelete = value;
+            }
+        }
+
+        private bool canSave = false;
+
+        public bool CanSave
+        {
+            get => canSave;
+            private set => SetProperty(ref canSave, value);
+        }
+
+        private bool canDelete = false;
+
+        public bool CanDelete
+        {
+            get => canDelete;
+            private set => SetProperty(ref canDelete, value);
+        }
+
+        #endregion Button enable/disable logic
+        #region Commands for buttons
+
+        public ICommand Save { get; private set; }
+        public ICommand Delete { get; private set; }
+        public ICommand Cancel { get; private set; }
+
+        #endregion Commands for buttons
+
+        public CustomerDetailsVM(CustomersRepo customersRepo)
+        {
+            this.customersRepo = customersRepo;            
+
+            Save = new AsyncRelayCommand(SaveCustomer);
+            Delete = new AsyncRelayCommand(DeleteCustomer);
+            Cancel = new RelayCommand(
+                () => WorkspaceNavUtils.NavigateTo(WorkspaceViewName.Customers)
+                );
+        }
+
+        public override async void LoadData(object? id = null)
+        {
+            if (id != null)
+            {
+                IsEditMode = true;
+                await LoadCustomer((string)id);
+            }
+
+            CanSave = true;
+        }
+
+        private async Task LoadCustomer(string id)
+        {
+            Customer customer = await customersRepo.GetCustomer(id);
+            Id = customer.Id;
+            Name = customer.Name;
+            Birthday = customer.Birthday;
+            Email = customer.Email;
+            Gender = customer.Gender;
+            Phone = customer.Phone;
+            Address = customer.Address;
+        }
+
+        private async Task SaveCustomer()
+        {
+            ValidateAllProperties();
+            if (HasErrors)
+                return;
+
+            var customer = new Customer()
+            {
+                Id = Id,
+                Name = Name,
+                Gender = Gender,
+                Birthday = Birthday,
+                Phone = Phone,
+                Email = Email,
+                Address = Address,
+            };
+
+            if (IsEditMode)
+            {
+                await customersRepo.UpdateCustomer(Id, customer);
+            }
+            else
+            {
+                await customersRepo.AddCustomer(customer);
+            }
+
+            // Navigate back to list screen
+            WorkspaceNavUtils.NavigateTo(WorkspaceViewName.Customers);
+        }
+
+        private async Task DeleteCustomer()
+        {
+            await customersRepo.DeleteCustomer(Id);
+            WorkspaceNavUtils.NavigateTo(WorkspaceViewName.Customers);
+        }
     }
 }
