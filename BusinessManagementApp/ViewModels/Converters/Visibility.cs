266bd5924242a3ac5e96caa5815823817e8fb6e7
Ha Phi Hung <haphihung55@gmail.com>
2023-01-03 03:51:08 +0700

    Fix collapsed delete button makes other buttons move to left

---
+++ BusinessManagementApp/ViewModels/Converters/Visibility.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace BusinessManagementApp.ViewModels.Converters
+{
+    /// <summary>
+    /// Better converter for bool to visibility.
+    /// Reference: https://stackoverflow.com/questions/3128023/wpf-booleantovisibilityconverter-that-converts-to-hidden-instead-of-collapsed-wh
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public sealed class BoolToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        public Visibility TrueValue { get; set; }
+        public Visibility FalseValue { get; set; }
+
+        public BoolToVisibilityConverter()
+        {
+            TrueValue = Visibility.Visible;
+            FalseValue = Visibility.Collapsed;
+        }
+
+        public object? Convert(object value,
+                               Type targetType,
+                               object parameter,
+                               CultureInfo culture)
+        {
+            if (!(value is bool))
+                return null;
+            return (bool)value ? TrueValue : FalseValue;
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (Equals(value, TrueValue))
+                return true;
+            if (Equals(value, FalseValue))
+                return false;
+            return null;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
